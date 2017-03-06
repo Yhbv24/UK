ed05ef4a3f61f103db9f74d59c0ac4e825656539
Felix Oporto Lopez and Leah Sherrell <felix@>
2017-03-06 09:12:03 -0800

    create US class shell

--- src/US.php
+++ src/US.php
@@ -1,4 +1,44 @@
 <?php
-    
+    class US
+    {
+        private $word;
+        private $definition;
+        private $example;
+        private $region;
+        private $id;
+
+        function __construct($word, $definition, $example, $region, $id=null)
+        {
+            $this->word = $word;
+            $this->definition = $definition;
+            $this->example = $example;
+            $this->region = $region;
+            $this->id = $id;
+        }
+
+        function getWord()
+        {
+
+        }
+
+        function getDefinition()
+        {
+
+        }
+
+        function getExample()
+        {
+
+        }
+
+        function getRegion()
+        {
+
+        }
+
+        function setId()
+        {
+
+        }
     }
  ?>
