2a79bcdcf20e86aa4265f5f7929284ceebf652fc
Ash Laidlaw and Sarah Leahy <sarah@>
2017-03-06 16:44:00 -0800

    Add basic form to add US and UK words, which adds them to the join table

--- app/app.php
+++ app/app.php
@@ -20,11 +20,49 @@
     // ***** Get routes *****
 
     $app->get("/", function() use ($app) { // Route to the home page
-        return $app["twig"]->render("index.html.twig", array("uk_words" => $found_uk_words));
+        // $uk_words = UK_word::getAll();
+        // $us_words = US_word::getAll();
+
+        return $app["twig"]->render("index.html.twig", array("search" => $search_word));
     });
+
     $app->post("/", function() use ($app) {
-        $search_word = $_POST["uk_word"];
-        $found_uk_words = $search_word->getUSWord();
+        $search_word = $_POST["us_word"];
+        UK_word::searchUSWords($search_word);
+
+        return $app['twig']->render("index.html.twig", array("search" => $search_word));
+    });
+
+    $app->get('/add_US_word', function() use ($app) {
+        return $app['twig']->render('add_us_word.html.twig');
+    });
+
+    $app->post('/add_US_word', function() use ($app) {
+        $us_word = $_POST['us_word'];
+        $us_definition = $_POST['definition'];
+        $us_example = $_POST['example'];
+        $us_region = $_POST['region'];
+        $new_word = new US_word($us_word, $us_definition, $us_example, $us_region);
+        $new_word->save();
+
+        $uk_word = $_POST['uk_word'];
+        $uk_definition = $_POST['uk_definition'];
+        $uk_example = $_POST['uk_example'];
+        $uk_region = $_POST['uk_region'];
+        $new_uk_word = new UK_word($uk_word, $uk_definition, $uk_example, $uk_region);
+        $new_uk_word->save();
+
+        $new_uk_word->addUSWord($new_word->getId());
+
         return $app->redirect('/');
     });
-return $app;
+
+    $app->post('/delete_all', function() use ($app) {
+        UK_word::deleteAll();
+        US_word::deleteAll();
+        $GLOBALS['DB']->exec("DELETE FROM UK_US;");
+
+        return $app->redirect("/");
+    });
+
+    return $app;
