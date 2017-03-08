77b0758df5887ba219b2918374723e9b83e3e4f6
Sarah Leahy <sleahy115@gmail.com>
2017-03-08 15:50:17 -0800

    isolate definition result

--- src/api.php
+++ src/api.php
@@ -2,27 +2,21 @@
 curl_init();
 $accept = "application/json";
 
-// if(!empty($_GET['definition'])){
-//     $dictionary_url = 'https://od-api.oxforddictionaries.com/api/v1'. $_GET['definition'] . '/regions=gb';
-//
-//     $dictionary_json = file_get_contents($dictionary_url);
-//     $definitions = json_decode($dictionary_json, true);
-//
-//     $definition = $result['definitions'][0];
-// };
-$definition = $_GET['search'];
+$search = $_GET['search'];
 $curl = curl_init();
-$dictionary_url = curl_setopt($curl, CURLOPT_URL, "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" . $definition . "/regions=gb");
+$dictionary_url = curl_setopt($curl, CURLOPT_URL, "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" . $search . "/regions=gb");
 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
 "Accept:". $accept, "app_id:" . "1ebc726a", "app_key:" . "693e18bfbed8265863ef38fc0a211262"));
 $result = curl_exec($curl);
 $exploded = explode("[",$result);
-var_dump($result);
-var_dump("exploded");
-var_dump($exploded[7]);
-$exploded_result = explode("]",$exploded);
+// var_dump($result);
+// var_dump("exploded");
+$definition = $exploded[7];
+var_dump($definition);
+$exploded_result = explode("]",$definition);
+$var_dump($exploded_result);
 curl_close($curl);
 
 ?>
